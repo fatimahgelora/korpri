be7ffb29f625b9f04988192df7836dd1fec3169d
failamir <38488480+failamir@users.noreply.github.com>
2025-08-19 03:36:07 +0700

    fixing

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import '../assets/scss/style.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
